31d59f109a9a1b7fda7f916fcd0b147342b5e0e5
rafaeldewandaru <rafael29204@gmail.com>
2023-09-13 10:36:16 +0700

    More updates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+def show_main(request):
+    context = {
+        'name': 'Rafael Bismo Dewandaru',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
 # Create your views here.
